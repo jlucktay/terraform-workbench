446cf45388f93619b8ea4d617be2b82f3c0f5ac3
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-04 19:41:47 +0100

    Added policy to backend S3 bucket which enforces encryption

--- backend/s3.tf
+++ backend/s3.tf
@@ -11,3 +11,8 @@ resource "aws_s3_bucket" "state-storage" {
     enabled = true
   }
 }
+
+resource "aws_s3_bucket_policy" "backend" {
+  bucket = "${aws_s3_bucket.state-storage.id}"
+  policy = "${file("s3.policy.json")}"
+}
