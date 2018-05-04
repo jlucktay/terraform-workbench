7eb384551145902d3ad8133fe56a9a92c4b75085
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-04 19:42:02 +0100

    Enabled server side encryption

--- backend/s3.tf
+++ backend/s3.tf
@@ -3,6 +3,14 @@ resource "aws_s3_bucket" "state-storage" {
   bucket = "james-lucktaylor-terraform"
   region = "${data.aws_region.current.name}"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags {
     Owner = "james.lucktaylor"
   }
