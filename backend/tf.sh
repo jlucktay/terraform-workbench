c2dc33f5b6173cf7ee2f940b172bbabc1a607b25
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-05 14:36:45 +0100

    Clean up plans too

--- backend/tf.sh
+++ backend/tf.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 awsume celab
-rm -rfv .terraform terraform.tfstate terraform.tfstate.backup .terraform.tfstate.lock.info
+rm -rfv .terraform terraform.tfstate terraform.tfstate.backup .terraform.tfstate.lock.info plan.*.out
 terraform init
 terraform import aws_s3_bucket.state-storage james-lucktaylor-terraform
 terraform import aws_dynamodb_table.state-locking-consistency james.lucktaylor.dynamodb.terraform
