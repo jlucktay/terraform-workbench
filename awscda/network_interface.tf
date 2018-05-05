056b35e940c4bcf433bb2f56f5d7bd4183416025
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-05 16:00:06 +0100

    Straightened out ENI for EC2

--- awscda/network_interface.tf
+++ awscda/network_interface.tf
@@ -1,22 +1,4 @@
-resource "aws_network_interface" "awscda" {
-  description = "Primary network interface"
-  subnet_id   = "${aws_instance.awscda.subnet_id}"
-
-  tags = "${merge(
-    local.default-tags,
-    map(
-      "Name", "james.lucktaylor.nic.awscda"
-    )
-  )}"
-}
-
-resource "aws_network_interface_attachment" "awscda" {
-  device_index         = 0
-  instance_id          = "${aws_instance.awscda.id}"
-  network_interface_id = "${aws_network_interface.awscda.id}"
-}
-
 resource "aws_network_interface_sg_attachment" "awscda-dmz" {
-  network_interface_id = "${aws_network_interface.awscda.id}"
-  security_group_id    = "${aws_security_group.dmz.id}"
+  network_interface_id = "${aws_instance.awscda.primary_network_interface_id}"
+  security_group_id    = "${data.aws_security_group.dmz.id}"
 }
