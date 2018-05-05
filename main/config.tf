6ee3d86ef542703516aaa125fe742f971178ddd1
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-05 02:13:41 +0100

    Added DynamoDB locking to backend config

--- main/config.tf
+++ main/config.tf
@@ -2,11 +2,12 @@ terraform {
   required_version = ">= 0.11.7"
 
   backend "s3" {
-    acl     = "private"
-    bucket  = "james-lucktaylor-terraform"
-    encrypt = true
-    key     = "terraform.tfstate"
-    region  = "eu-west-1"
+    acl            = "private"
+    bucket         = "james-lucktaylor-terraform"
+    dynamodb_table = "james.lucktaylor.dynamodb.terraform"
+    encrypt        = true
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
   }
 }
 
