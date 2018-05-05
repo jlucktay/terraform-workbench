5eba2cbe3e78a718d08c586f479e60c0529741ac
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-05 15:59:36 +0100

    Needed some more lookups

--- awscda/data.tf
+++ awscda/data.tf
@@ -1,3 +1,7 @@
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {}
+
 data "aws_ami" "amazon-linux-latest" {
   filter {
     name   = "name"
@@ -5,4 +9,18 @@ data "aws_ami" "amazon-linux-latest" {
   }
 
   most_recent = true
+}
+
+data "aws_security_group" "dmz" {
+  filter {
+    name   = "tag:Name"
+    values = ["james.lucktaylor.sg.dmz"]
+  }
+}
+
+data "aws_subnet" "main-a" {
+  filter {
+    name   = "tag:Name"
+    values = ["james.lucktaylor.subnet.a"]
+  }
 }
