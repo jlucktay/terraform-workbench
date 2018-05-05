f1ab392c21b05e6e8460bbcaa04863d3cd9f03c3
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-05 14:32:46 +0100

    Updated resource type to bring default SG in created VPC under control

--- infra/security_group.default.tf
+++ infra/security_group.default.tf
@@ -1,8 +1,19 @@
-resource "aws_security_group" "default" {
-  description            = "default VPC security group"
-  name                   = "default"
-  revoke_rules_on_delete = true
-  vpc_id                 = "${aws_vpc.main.id}"
+resource "aws_default_security_group" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = 0
+    protocol  = -1
+    self      = true
+    to_port   = 0
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  }
 
   tags = "${merge(
     local.default-tags,
@@ -10,32 +21,9 @@ resource "aws_security_group" "default" {
   )}"
 }
 
-resource "aws_security_group_rule" "default-allow-http-from-everywhere" {
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "HTTP from everywhere"
-  from_port         = 80
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.default.id}"
-  to_port           = 80
-  type              = "ingress"
-}
-
-resource "aws_security_group_rule" "default-allow-all-from-self" {
-  description       = "All from self"
-  from_port         = -1
-  protocol          = "all"
-  security_group_id = "${aws_security_group.default.id}"
-  self              = true
-  to_port           = -1
-  type              = "ingress"
-}
-
-resource "aws_security_group_rule" "default-allow-all-to-everywhere" {
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "All to everywhere"
-  from_port         = -1
-  protocol          = "all"
-  security_group_id = "${aws_security_group.default.id}"
-  to_port           = -1
-  type              = "egress"
-}
+# resource "aws_security_group" "default" {
+#   description            = "default VPC security group"
+#   name                   = "default"
+#   revoke_rules_on_delete = true
+#   vpc_id                 = "${aws_vpc.main.id}"
+# }
