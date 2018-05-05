1353da8a5be2d66c083869295948def8acd42f1a
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-05 14:20:37 +0100

    Changed resource type to control default NACL in created VPC

--- infra/network_acl.tf
+++ infra/network_acl.tf
@@ -1,5 +1,24 @@
-resource "aws_network_acl" "main" {
-  vpc_id = "${aws_vpc.main.id}"
+resource "aws_default_network_acl" "main" {
+  default_network_acl_id = "${aws_vpc.main.default_network_acl_id}"
+  subnet_ids             = ["${aws_subnet.main.*.id}"]
+
+  ingress {
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    protocol   = -1
+    rule_no    = 100
+    to_port    = 0
+  }
+
+  egress {
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    protocol   = -1
+    rule_no    = 100
+    to_port    = 0
+  }
 
   tags = "${merge(
     local.default-tags,
