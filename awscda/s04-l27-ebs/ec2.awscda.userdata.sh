9b3c3d6948e8f2590b92394e247f3298a2e02565
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-07 23:17:21 +0100

    Script for formatting, file system creation, mounting of volumes

--- awscda/s04-l27-ebs/ec2.awscda.userdata.sh
+++ awscda/s04-l27-ebs/ec2.awscda.userdata.sh
@@ -2,3 +2,16 @@
 
 sudo yum update -y
 echo "alias lsl='ls -Al'" >> ~ec2-user/.bashrc
+
+drives=( sdb sdc sdd )
+
+for drive in "${drives[@]}"
+do
+    sudo parted --script "/dev/$drive" \
+        mklabel gpt \
+        mkpart primary ext4 0% 100%
+
+    sudo mkfs.ext4 -F "/dev/$drive"
+    sudo mkdir -pv "/mnt/$drive"
+    sudo mount "/dev/$drive" "/mnt/$drive"
+done
