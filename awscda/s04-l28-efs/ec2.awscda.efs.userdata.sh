1e406d46de6c0d66719f1309c285b2e692c448c1
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-18 16:42:04 +0100

    Refactored some more

--- awscda/s04-l28-efs/ec2.awscda.efs.userdata.sh
+++ awscda/s04-l28-efs/ec2.awscda.efs.userdata.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+# Capture user-data kickoff timestamp
+TIMESTAMP=$(date '+%Y%m%d.%H%M%S.%N%z')
+
+# Do some boilerplate setup
 sudo yum update -y
 echo "alias lsl='ls -Al'" >> ~ec2-user/.bashrc
+
+# Get some details ready for later
+sudo yum install -y jq
+MY_IP=$(curl --silent httpbin.org/ip | jq -r '.origin')
 
 # Sort out Apache (httpd)
 sudo yum install -y httpd
 service httpd start
 sudo chkconfig httpd on
 
-# Sort out EFS volume
+# Sort out EFS volume mount
 sudo yum install -y amazon-efs-utils
 echo "!!EFS_ID!! /var/www/html efs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
-
-# Create/append to web page
-sudo yum install -y jq
-TIMESTAMP=$(date '+%Y%m%d.%H%M%S.%N%z')
-MY_IP=$(curl --silent httpbin.org/ip | jq -r '.origin')
 
 # Keep attempting to mount EFS, until it succeeds
 until sudo mount /var/www/html; do
     sleep 1
 done
 
+# Create/append to web page, with timestamp and IP
 echo "[${TIMESTAMP}] ${MY_IP}<br />" | sudo tee -a /var/www/html/index.html
