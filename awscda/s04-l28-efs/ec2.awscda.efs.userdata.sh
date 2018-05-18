777b2ca61598b4ebb862c6e6f56826f2ab021f4e
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-18 16:10:53 +0100

    Resequenced to optimise for time

--- awscda/s04-l28-efs/ec2.awscda.efs.userdata.sh
+++ awscda/s04-l28-efs/ec2.awscda.efs.userdata.sh
@@ -12,13 +12,14 @@ sudo chkconfig httpd on
 sudo yum install -y amazon-efs-utils
 echo "!!EFS_ID!! /var/www/html efs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
 
+# Create/append to web page
+sudo yum install -y jq
+TIMESTAMP=$(date '+%Y%m%d.%H%M%S%z')
+MY_IP=$(curl --silent httpbin.org/ip | jq -r '.origin')
+
 # Keep attempting to mount EFS, until it succeeds
 until sudo mount /var/www/html; do
     sleep 1
 done
 
-# Create/append to web page
-sudo yum install -y jq
-TIMESTAMP=$(date '+%Y%m%d.%H%M%S%z')
-MY_IP=$(curl --silent httpbin.org/ip | jq -r '.origin')
 echo "[${TIMESTAMP}] ${MY_IP}<br />" | sudo tee -a /var/www/html/index.html
