e050e2ad1037264bf7debb7eaf79f1e60f9ca29b
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-23 11:33:02 +0100

    Want public IPs on EC2s

--- infra/subnet.tf
+++ infra/subnet.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "main" {
   availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
   cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 1)}"
   count                   = "${length(data.aws_availability_zones.available.names)}"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
   vpc_id                  = "${aws_vpc.main.id}"
 
   tags = "${merge(
