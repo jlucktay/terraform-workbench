832f0ef77533b3e509878f00ddfee8ea8ffee870
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-23 11:32:47 +0100

    Don't need 'sudo' in user-data

--- awscda/s04-l27-ebs/ec2.awscda.ebs.userdata.sh
+++ awscda/s04-l27-ebs/ec2.awscda.ebs.userdata.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sudo yum update -y
+yum update -y
 echo "alias lsl='ls -Al'" >> ~ec2-user/.bashrc
 
 drives=( sdb sdc sdd )
 
 for drive in "${drives[@]}"
 do
-    sudo parted --script "/dev/$drive" \
+    parted --script "/dev/$drive" \
         mklabel gpt \
         mkpart primary ext4 0% 100%
 
-    sudo mkfs.ext4 -F "/dev/$drive"
-    sudo mkdir -p "/mnt/$drive"
-    echo "/dev/$drive   /mnt/$drive   ext4   defaults   0   2" | sudo tee -a /etc/fstab
-    sudo mount "/mnt/$drive"
+    mkfs.ext4 -F "/dev/$drive"
+    mkdir -p "/mnt/$drive"
+    echo "/dev/$drive   /mnt/$drive   ext4   defaults   0   2" | tee -a /etc/fstab
+    mount "/mnt/$drive"
 done
