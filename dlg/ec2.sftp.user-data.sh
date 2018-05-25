ac8766f7d10fc7fd50eb532beaf2f5623c537443
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-25 11:36:51 +0100

    Working on user-data. Have the public key being pulled from Secrets Manager now.

--- dlg/ec2.sftp.user-data.sh
+++ dlg/ec2.sftp.user-data.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-echo "alias lsl='ls -Al'" >> ~ec2-user/.bashrc
-echo "alias lsl='ls -Al'" >> ~root/.bashrc
+set -euo pipefail
+IFS=$'\n\t'
 
 # Drop a note when this script is done (note: 'done' might include exiting prematurely due to an error!)
 trap "echo DONE >> ~ec2-user/user-data.log" INT TERM EXIT
+
+# Some helpful aliases
+echo "alias lsl='ls -Al'" >> ~ec2-user/.bashrc
+echo "alias lsl='ls -Al'" >> ~root/.bashrc
 
 # Update all of the things
 yum update -y
@@ -44,9 +47,17 @@ mkfs -t ext4 /dev/xvdb
 echo "/dev/xvdb   /home/sftp   ext4   defaults,nofail   0   2" | tee -a /etc/fstab
 mount -a
 
-# Put the public key into ~sftp
+# Get AWS CLI
+curl https://bootstrap.pypa.io/get-pip.py --output /tmp/get-pip.py
+python /tmp/get-pip.py --user
+echo "export PATH=~/.local/bin:\$PATH" >> ~/.bashrc
+PATH=~/.local/bin:$PATH
+pip install awscli --upgrade --user
+
+# Get public key from Secrets Manager and put it into ~sftp
+PUBKEY=$(aws secretsmanager get-secret-value --secret-id james.lucktaylor.fa-sftp.pub --region eu-west-1 | grep SecretString | cut -d\\ -f4 | cut -d'"' -f2)
 mkdir ~sftp/.ssh
-echo "I'm a public key!" >> ~sftp/.ssh/authorized_keys
+echo "$PUBKEY" >> ~sftp/.ssh/authorized_keys
 
 # Fix file/directory permissions in ~sftp
 chown -Rc sftp:sftp ~sftp
@@ -58,85 +69,15 @@ chmod -c 600 ~sftp/.ssh/authorized_keys
 
 # # ### /dev/xvdf        99G   61M   94G   1% /home/sftp
 
-# # cd .ssh/
-# # cd /etc/ssh/
-# # cd /home/sftp/
-# # cd /tmp/sftp/
-# # cd ~sftp/
 
-# # chmod -Rc ug+rw /mnt/sftp
-# # chown -c sftp.sftp .ssh
-# # chown -c sftp.sftp authorized_keys
-# # chown -R sftp:sftp /mnt/sftp
 
-# # cp -ivr ~sftp/.* /tmp/sftp/
-# # cp -ivrt /home/sftp/ .ssh
-# # cp -ivt /home/sftp/ ./.bash* .ssh/ test2.txt Test.txt
-# # cp /etc/fstab /etc/fstab.20180523
-# # cp /etc/sudoers /etc/sudoers.bkp
-# # date
-# # df -h
-# # exit
-# # file -s /dev/xvda1
-# # file -s /dev/xvda2
-# # file -s /dev/xvdf
-# # history
-# # hostname
 # # less sshd_config
-# # ll
-# # locate nano
-# # ls
-# # ls -Al
-# # ls -Al ~sftp/
-# # ls /mnt/sftp/
-# # lsblk
-# # lsl
-# # lsl -a /mnt/sftp/
-# # lsl /home/
-# # lsl /home/sftp/
-# # lsl /mnt/
-# # lsl /mnt/sftp/
-# # lsl ~sftp/
-# # man adduser
-# # man useradd
-# # mkdir -pv /mnt/sftp
-# # mkdir .ssh
-# # mount
-# # mount -a
-# # mount /dev/xvdf /mnt/sftp
-# # mount /home/sftp
-# # nano authorized_keys
-# # passwd dlgfraudanalnprdL2
-# # ping 10.74.0.100
-# # pwd
-# # rm -rfv devopsadmin/ dlgfraudanalnprdL2/ ec2-user/
-# # route
-# # service --status-all
-
-# # sudo cat ~ec2-user/.ssh/authorized_keys
-# # sudo cat ~sftp/.ssh/authorized_keys
-# # sudo ls -Al ~sftp
-# # sudo ls -Al ~sftp/.ssh
-# # sudo lsl ~sftp/
 # # sudo nano /etc/ssh/sshd_config
-# # sudo passwd devopsadmin
-# # sudo su
-# # sudo su -
 # # sudo systemctl restart sshd.service
 # # sudo systemctl status sshd.service
-# # sudo useradd devopsadmin
 # # sudo vi /etc/ssh/sshd_config
 # # sudo vim /etc/ssh/sshd_config
 # # sudo visudo
-# # systemctl
-# # systemctl --help
 # # systemctl restart sshd.service
 # # systemctl sshd.service
 # # systemctl status sshd.service
-# # umount /mnt/sftp
-# # useradd devopsadmin
-# # useradd dlgfraudanalnprdL2
-# # vi /etc/fstab
-# # vi /etc/sudoers
-# # vi authorized_keys
-# # visudo
