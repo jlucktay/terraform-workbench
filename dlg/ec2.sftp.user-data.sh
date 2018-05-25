839a2fc210bfa9c508d0ebd73d7be5ad424fd2bd
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-25 12:20:12 +0100

    Added notes around possible SSHD config. Cleaned up plaintext secret on both sides.

--- dlg/ec2.sftp.user-data.sh
+++ dlg/ec2.sftp.user-data.sh
@@ -5,6 +5,21 @@ IFS=$'\n\t'
 # Drop a note when this script is done (note: 'done' might include exiting prematurely due to an error!)
 trap "echo DONE >> ~ec2-user/user-data.log" INT TERM EXIT
 
+### CONFIGURATION
+# The ID of the secret within Secrets Manager, which holds the public key that Globalscape will connect with
+SecretId="james.lucktaylor.fa-sftp.pub"
+# Note that this is not the fully-qualified ARN, only the name of the secret
+
+### TODO
+# The '/etc/ssh/sshd_config' file might need some finessing, to comply with how the Globalscape client connects to this VM
+#
+# EXAMPLE
+# # echo PutYourConfigChangesHere >> /etc/ssh/sshd_config
+#
+# Make sure to restart the SSHD service after modifying its config.
+# Use the following command to do the restart:
+# # systemctl restart sshd.service
+
 # Some helpful aliases
 echo "alias lsl='ls -Al'" >> ~ec2-user/.bashrc
 echo "alias lsl='ls -Al'" >> ~root/.bashrc
@@ -27,57 +42,24 @@ parted --script "/dev/xvdb" \
 
 mkfs -t ext4 /dev/xvdb
 
-
-
-# # Wait for the second UUID to show up
-# cmd_find () {
-#     find /dev/disk/by-uuid/ -type l -ilname "*xvd*" | wc -l
-# }
-
-# until (( $(cmd_find) == 2 )); do
-# 	sleep 1
-# done
-
-# # Get the UUID of the extra volume
-# DISKUUID=$(find /dev/disk/by-uuid/ -type l -ilname "*xvdb*" | cut -d'/' -f5)
-
-
-
-# Add a line for the extra volume to fstab, and mount
+# Add a line for the extra volume to fstab, and mount it ('-a' mounts _everything_; RTFM for more detail)
 echo "/dev/xvdb   /home/sftp   ext4   defaults,nofail   0   2" | tee -a /etc/fstab
 mount -a
 
-# Get AWS CLI
+# Install the AWS CLI
 curl https://bootstrap.pypa.io/get-pip.py --output /tmp/get-pip.py
 python /tmp/get-pip.py --user
 echo "export PATH=~/.local/bin:\$PATH" >> ~/.bashrc
 PATH=~/.local/bin:$PATH
 pip install awscli --upgrade --user
 
-# Get public key from Secrets Manager and put it into ~sftp
-PUBKEY=$(aws secretsmanager get-secret-value --secret-id james.lucktaylor.fa-sftp.pub --region eu-west-1 | grep SecretString | cut -d\\ -f4 | cut -d'"' -f2)
+# Get the public key from Secrets Manager and put it into ~sftp
+PublicKey=$(aws secretsmanager get-secret-value --secret-id $SecretId --region eu-west-1 | grep SecretString | cut -d'"' -f4)
 mkdir ~sftp/.ssh
-echo "$PUBKEY" >> ~sftp/.ssh/authorized_keys
+echo "$PublicKey" >> ~sftp/.ssh/authorized_keys
 
 # Fix file/directory permissions in ~sftp
 chown -Rc sftp:sftp ~sftp
 chmod -Rc ug+rw ~sftp
 chmod -c 700 ~sftp/.ssh
 chmod -c 600 ~sftp/.ssh/authorized_keys
-
-
-
-# # ### /dev/xvdf        99G   61M   94G   1% /home/sftp
-
-
-
-# # less sshd_config
-# # sudo nano /etc/ssh/sshd_config
-# # sudo systemctl restart sshd.service
-# # sudo systemctl status sshd.service
-# # sudo vi /etc/ssh/sshd_config
-# # sudo vim /etc/ssh/sshd_config
-# # sudo visudo
-# # systemctl restart sshd.service
-# # systemctl sshd.service
-# # systemctl status sshd.service
