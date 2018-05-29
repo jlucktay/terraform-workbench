3a2702ddd2d653ab1047c39494ead44b8ba0370c
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-05-29 17:13:09 +0100

    Added another policy to same EC2 IAM role. EC2 revokes its own permissions after fetching the secret.

--- dlg/ec2.sftp.user-data.sh
+++ dlg/ec2.sftp.user-data.sh
@@ -7,7 +7,7 @@ trap "echo DONE >> ~ec2-user/user-data.log" INT TERM EXIT
 
 ### CONFIGURATION
 # The ID of the secret within Secrets Manager, which holds the public key that Globalscape will connect with
-SecretId="james.lucktaylor.fa-sftp.pub"
+SecretId="fa-sftp-ec2"
 # Note that this is not the fully-qualified ARN, only the name of the secret
 
 ### TODO
@@ -46,20 +46,26 @@ mkfs -t ext4 /dev/xvdb
 echo "/dev/xvdb   /home/sftp   ext4   defaults,nofail   0   2" | tee -a /etc/fstab
 mount -a
 
-# Install the AWS CLI
+# Install and configure the AWS CLI
 curl https://bootstrap.pypa.io/get-pip.py --output /tmp/get-pip.py
 python /tmp/get-pip.py --user
 echo "export PATH=~/.local/bin:\$PATH" >> ~/.bashrc
 PATH=~/.local/bin:$PATH
 pip install awscli --upgrade --user
+aws configure set default.region eu-west-1
 
 # Get the public key from Secrets Manager and put it into ~sftp
-PublicKey=$(aws secretsmanager get-secret-value --secret-id $SecretId --region eu-west-1 | grep SecretString | cut -d'"' -f4)
+PublicKey=$(aws secretsmanager get-secret-value --secret-id $SecretId | grep SecretString | cut -d'"' -f4)
 mkdir ~sftp/.ssh
 echo "$PublicKey" >> ~sftp/.ssh/authorized_keys
 
-# Fix file/directory permissions in ~sftp
+# Fix file/directory permissions in ~sftp since it was re-homed to a new volume
 chown -Rc sftp:sftp ~sftp
 chmod -Rc ug+rw ~sftp
 chmod -c 700 ~sftp/.ssh
 chmod -c 600 ~sftp/.ssh/authorized_keys
+
+# Revoke Secrets Manager access by removing the IAM role from the EC2
+InstanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+AssociationId=$(aws ec2 describe-iam-instance-profile-associations --query "IamInstanceProfileAssociations[?InstanceId=='$InstanceId']" | grep AssociationId | cut -d'"' -f4)
+aws ec2 disassociate-iam-instance-profile --association-id "$AssociationId"
--- dlg/iam.tf
+++ dlg/iam.tf
@@ -41,3 +41,32 @@ data "aws_iam_policy_document" "ec2-access-secrets-doc" {
     resources = ["${data.aws_secretsmanager_secret.fa-sftp-ec2.arn}"]
   }
 }
+
+resource "aws_iam_role_policy_attachment" "ec2-instance-profile-attach" {
+  role       = "${aws_iam_role.fa-sftp-ec2.name}"
+  policy_arn = "${aws_iam_policy.ec2-instance-profile-policy.arn}"
+}
+
+resource "aws_iam_policy" "ec2-instance-profile-policy" {
+  name        = "ec2-instance-profile-policy"
+  description = "Allows the FA SFTP EC2 to describe and disassociate instance profiles, so that it can revoke its access after it has performed the necessary user-data steps."
+  policy      = "${data.aws_iam_policy_document.ec2-instance-profile-doc.json}"
+}
+
+data "aws_iam_policy_document" "ec2-instance-profile-doc" {
+  statement {
+    actions = ["ec2:DescribeIamInstanceProfileAssociations"]
+
+    effect = "Allow"
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = ["ec2:DisassociateIamInstanceProfile"]
+
+    effect = "Allow"
+
+    resources = ["arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:instance/${aws_instance.fa-sftp-ec2.id}"]
+  }
+}
