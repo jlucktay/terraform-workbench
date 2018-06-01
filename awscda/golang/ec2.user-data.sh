87d94063325cf838a4f8f47d42dd0d2d8bce70ee
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-06-01 11:48:55 +0100

    WIP Golang dev box

---
+++ awscda/golang/ec2.user-data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Drop a note when this script is done (note: 'done' might include exiting prematurely due to an error!)
+trap "echo DONE >> ~ec2-user/user-data.log" INT TERM EXIT
+
+# Some helpful aliases
+echo "alias lsl='ls -Al'" >> ~ec2-user/.bashrc
+echo "alias lsl='ls -Al'" >> ~root/.bashrc
+
+# Update all of the things
+yum update -y
+
+# Go!
+yum install -y golang
+cd ~ec2-user
+su ec2-user -c "go get -u -v github.com/aws/aws-sdk-go/..."
