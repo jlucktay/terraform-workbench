6276e59f0d1ac7d139b6a4a95f84d7a57ca8aa22
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-06-01 11:49:25 +0100

    WIP

--- awscda/s04-l31-iam/role.tf
+++ awscda/s04-l31-iam/role.tf
@@ -1,7 +1,17 @@
-resource "aws_iam_role" "main" {
-  name = "james.lucktaylor.awscda.iam.ec2"
-}
-
-# resource "aws_iam_service_linked_role" "main" {
-#   aws_service_name = "ec2.amazonaws.com"
+# resource "aws_iam_role" "main" {
+#   assume_role_policy = "${data.aws_iam_policy_document.ec2-assume-role-doc.json}"
+#   name               = "james.lucktaylor.awscda.iam.ec2"
+# }
+# # resource "aws_iam_service_linked_role" "main" {
+# #   aws_service_name = "ec2.amazonaws.com"
+# # }
+# data "aws_iam_policy_document" "ec2-assume-role-doc" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     effect = "Allow"
+#     principals {
+#       identifiers = ["ec2.amazonaws.com"]
+#       type        = "Service"
+#     }
+#   }
 # }
