a036b93140fbbf5c3b39d5f269dec41aad7f136b
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-06-07 09:39:34 +0100

    Switched to own key (from ~/.ssh/)

--- infra/key_pair.tf
+++ infra/key_pair.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "eu-west-1" {
   key_name   = "james.lucktaylor.eu-west-1"
-  public_key = "${file("james.lucktaylor.eu-west-1.pub")}"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
 # TODO: set up keys across multiple regions
