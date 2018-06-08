8cb724f8de205b80e9fe796d9c0676d89d93b5a1
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-06-08 17:04:25 +0100

    Added more verbose description

--- awscda/s04-l27-ebs/var.tf
+++ awscda/s04-l27-ebs/var.tf
@@ -1,4 +1,4 @@
 variable "region" {
   default     = ""
-  description = "Which AWS region to operate in?"
+  description = "Which AWS region to operate in? Defaults to blank, so that prompts/errors will occur"
 }
