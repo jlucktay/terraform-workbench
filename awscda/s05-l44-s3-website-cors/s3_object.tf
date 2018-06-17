debf4b10bea319a2f1efd82fc9c76b88ed2801c6
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-06-17 13:55:52 +0100

    Using 'content' instead of 'source' as it catches changes within files

--- awscda/s05-l44-s3-website-cors/s3_object.tf
+++ awscda/s05-l44-s3-website-cors/s3_object.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket_object" "website-index" {
   acl          = "public-read"
   bucket       = "${aws_s3_bucket.website-index.bucket}"
+  content      = "${file("./index.html")}"
   content_type = "text/html"
   key          = "${aws_s3_bucket.website-index.website.0.index_document}"
-  source       = "./index.html"
 
   tags = "${
     merge(
@@ -15,9 +15,9 @@ resource "aws_s3_bucket_object" "website-index" {
 resource "aws_s3_bucket_object" "website-error" {
   acl          = "public-read"
   bucket       = "${aws_s3_bucket.website-index.bucket}"
+  content      = "${file("./error.html")}"
   content_type = "text/html"
   key          = "${aws_s3_bucket.website-index.website.0.error_document}"
-  source       = "./error.html"
 
   tags = "${
     merge(
@@ -29,9 +29,9 @@ resource "aws_s3_bucket_object" "website-error" {
 resource "aws_s3_bucket_object" "cors-page" {
   acl          = "public-read"
   bucket       = "${aws_s3_bucket.website-cors.bucket}"
+  content      = "${file("./loadpage.html")}"
   content_type = "text/html"
   key          = "${aws_s3_bucket.website-cors.website.0.index_document}"
-  source       = "./loadpage.html"
 
   tags = "${
     merge(
