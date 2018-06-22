0b8a470e29536e9f4b2adbd6499f59c00fca5bb2
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-06-22 19:04:57 +0100

    Refactored

--- projects/px1021/iam/var.tf
+++ projects/px1021/iam/var.tf
@@ -1,12 +1,9 @@
-# Following a pattern from here:
-# https://serialseb.com/blog/2016/05/11/terraform-working-around-no-count-on-module/
+variable "region" {
+  default     = ""
+  description = "Which AWS region to operate in? Defaults to blank, so that prompts/errors will occur"
+}
 
 variable "role-delimiter" {
   default     = " "
   description = "The character to delimit the input roles by (workaround for modules not supporting 'count')"
-}
-
-variable "region" {
-  default     = ""
-  description = "Which AWS region to operate in? Defaults to blank, so that prompts/errors will occur"
 }
