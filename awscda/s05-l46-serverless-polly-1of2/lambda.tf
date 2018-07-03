5f5b081336c12ae8fe43e916083a784c28761451
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-07-03 14:01:46 +0100

    Renamed first function, and added a third

--- awscda/s05-l46-serverless-polly-1of2/lambda.tf
+++ awscda/s05-l46-serverless-polly-1of2/lambda.tf
@@ -1,30 +1,30 @@
-resource "aws_lambda_function" "polly_new_posts" {
-  filename         = "${data.archive_file.polly_new_posts.output_path}"
-  function_name    = "james-lucktaylor-awscda-postreader-new_posts"
-  handler          = "new_posts.lambda_handler"
+resource "aws_lambda_function" "polly_new_post" {
+  filename         = "${data.archive_file.polly_new_post.output_path}"
+  function_name    = "james-lucktaylor-awscda-postreader-new_post"
+  handler          = "new_post.lambda_handler"
   role             = "${aws_iam_role.lambda_polly.arn}"
   runtime          = "python3.6"
-  source_code_hash = "${data.archive_file.polly_new_posts.output_base64sha256}"
+  source_code_hash = "${data.archive_file.polly_new_post.output_base64sha256}"
 
   tags = "${merge(
     local.default-tags,
     map(
       "Description", "james.lucktaylor - AWS CDA course on Udemy - section 5 lab 46 - Polly - This function creates posts in DynamoDB",
-      "Name", "james-lucktaylor-awscda-postreader-new_posts",
+      "Name", "james-lucktaylor-awscda-postreader-new_post",
     )
   )}"
 
   environment {
     variables {
       DB_TABLE_NAME = "${aws_dynamodb_table.posts.name}"
-      SNS_TOPIC     = "${aws_sns_topic.new_posts.arn}"
+      SNS_TOPIC     = "${aws_sns_topic.new_post.arn}"
     }
   }
 }
 
-data "archive_file" "polly_new_posts" {
-  output_path = "new_posts.zip"
-  source_file = "new_posts.py"
+data "archive_file" "polly_new_post" {
+  output_path = "new_post.zip"
+  source_file = "new_post.py"
   type        = "zip"
 }
 
@@ -63,5 +63,44 @@ resource "aws_lambda_permission" "convert_to_audio" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.polly_convert_to_audio.function_name}"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_sns_topic.new_posts.arn}"
+  source_arn    = "${aws_sns_topic.new_post.arn}"
 }
+
+resource "aws_lambda_function" "polly_get_posts" {
+  filename         = "${data.archive_file.polly_get_posts.output_path}"
+  function_name    = "james-lucktaylor-awscda-postreader-get_posts"
+  handler          = "get_posts.lambda_handler"
+  role             = "${aws_iam_role.lambda_polly.arn}"
+  runtime          = "python3.6"
+  source_code_hash = "${data.archive_file.polly_get_posts.output_base64sha256}"
+  timeout          = 300
+
+  tags = "${merge(
+    local.default-tags,
+    map(
+      "Description", "james.lucktaylor - AWS CDA course on Udemy - section 5 lab 46 - Polly - This function converts my text to audio files and saves them to S3",
+      "Name", "james-lucktaylor-awscda-postreader-get_posts",
+    )
+  )}"
+
+  environment {
+    variables {
+      DB_TABLE_NAME = "${aws_dynamodb_table.posts.name}"
+    }
+  }
+}
+
+data "archive_file" "polly_get_posts" {
+  output_path = "get_posts.zip"
+  source_file = "get_posts.py"
+  type        = "zip"
+}
+
+/*
+resource "aws_lambda_permission" "get_posts" {
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.polly_convert_to_audio.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_sns_topic.new_post.arn}"
+}
+*/
