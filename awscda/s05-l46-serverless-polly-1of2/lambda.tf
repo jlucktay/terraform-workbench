605e6316ca063b16386f5fdd4256f4b14a48ab95
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-07-03 13:57:07 +0100

    Added a second Lambda function, with an SNS subcription to trigger it

--- awscda/s05-l46-serverless-polly-1of2/lambda.tf
+++ awscda/s05-l46-serverless-polly-1of2/lambda.tf
@@ -1,16 +1,16 @@
-resource "aws_lambda_function" "polly_newposts" {
-  filename         = "${data.archive_file.polly_newposts.output_path}"
-  function_name    = "james-lucktaylor-awscda-postreader-newposts"
-  handler          = "newposts.lambda_handler"
+resource "aws_lambda_function" "polly_new_posts" {
+  filename         = "${data.archive_file.polly_new_posts.output_path}"
+  function_name    = "james-lucktaylor-awscda-postreader-new_posts"
+  handler          = "new_posts.lambda_handler"
   role             = "${aws_iam_role.lambda_polly.arn}"
   runtime          = "python3.6"
-  source_code_hash = "${data.archive_file.polly_newposts.output_base64sha256}"
+  source_code_hash = "${data.archive_file.polly_new_posts.output_base64sha256}"
 
   tags = "${merge(
     local.default-tags,
     map(
       "Description", "james.lucktaylor - AWS CDA course on Udemy - section 5 lab 46 - Polly - This function creates posts in DynamoDB",
-      "Name", "james-lucktaylor-awscda-postreader-newposts",
+      "Name", "james-lucktaylor-awscda-postreader-new_posts",
     )
   )}"
 
@@ -22,15 +22,46 @@ resource "aws_lambda_function" "polly_newposts" {
   }
 }
 
-data "archive_file" "polly_newposts" {
-  output_path = "newposts.zip"
-  source_file = "newposts.py"
+data "archive_file" "polly_new_posts" {
+  output_path = "new_posts.zip"
+  source_file = "new_posts.py"
   type        = "zip"
 }
 
-# resource "aws_lambda_permission" "serverless" {
-#   action        = "lambda:InvokeFunction"
-#   function_name = "${aws_lambda_function.serverless.function_name}"
-#   principal     = "apigateway.amazonaws.com"
-#   source_arn    = "${aws_api_gateway_rest_api.serverless.execution_arn}/*/GET/${aws_api_gateway_resource.serverless.path_part}"
-# }
+resource "aws_lambda_function" "polly_convert_to_audio" {
+  filename         = "${data.archive_file.polly_convert_to_audio.output_path}"
+  function_name    = "james-lucktaylor-awscda-postreader-convert_to_audio"
+  handler          = "convert_to_audio.lambda_handler"
+  role             = "${aws_iam_role.lambda_polly.arn}"
+  runtime          = "python2.7"
+  source_code_hash = "${data.archive_file.polly_convert_to_audio.output_base64sha256}"
+  timeout          = 300
+
+  tags = "${merge(
+    local.default-tags,
+    map(
+      "Description", "james.lucktaylor - AWS CDA course on Udemy - section 5 lab 46 - Polly - This function converts my text to audio files and saves them to S3",
+      "Name", "james-lucktaylor-awscda-postreader-convert_to_audio",
+    )
+  )}"
+
+  environment {
+    variables {
+      BUCKET_NAME   = "${aws_s3_bucket.polly_mp3s.bucket}"
+      DB_TABLE_NAME = "${aws_dynamodb_table.posts.name}"
+    }
+  }
+}
+
+data "archive_file" "polly_convert_to_audio" {
+  output_path = "convert_to_audio.zip"
+  source_file = "convert_to_audio.py"
+  type        = "zip"
+}
+
+resource "aws_lambda_permission" "convert_to_audio" {
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.polly_convert_to_audio.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_sns_topic.new_posts.arn}"
+}
--- awscda/s05-l46-serverless-polly-1of2/sns.tf
+++ awscda/s05-l46-serverless-polly-1of2/sns.tf
@@ -2,3 +2,9 @@ resource "aws_sns_topic" "new_posts" {
   display_name = "New Posts"
   name         = "james_lucktaylor_awscda_new_posts"
 }
+
+resource "aws_sns_topic_subscription" "lambda_polly_convert_to_audio" {
+  endpoint  = "${aws_lambda_function.polly_convert_to_audio.arn}"
+  protocol  = "lambda"
+  topic_arn = "${aws_sns_topic.new_posts.arn}"
+}
