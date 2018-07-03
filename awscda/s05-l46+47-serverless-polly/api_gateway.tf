63992888874b946e66b94d8f25f76b66c368cffe
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-07-03 17:54:43 +0100

    Set up GET method on API Gateway

--- awscda/s05-l46+47-serverless-polly/api_gateway.tf
+++ awscda/s05-l46+47-serverless-polly/api_gateway.tf
@@ -1,4 +1,3 @@
-/*
 resource "aws_api_gateway_rest_api" "polly" {
   description = "james.lucktaylor - Polly post reader"
   name        = "james.lucktaylor.awscda.polly"
@@ -8,35 +7,32 @@ resource "aws_api_gateway_rest_api" "polly" {
   }
 }
 
-resource "aws_api_gateway_method" "serverless" {
+resource "aws_api_gateway_resource" "polly" {
+  parent_id   = "${aws_api_gateway_rest_api.polly.root_resource_id}"
+  path_part   = "polly"
+  rest_api_id = "${aws_api_gateway_rest_api.polly.id}"
+}
+
+resource "aws_api_gateway_method" "polly_get" {
   authorization = "NONE"
   http_method   = "GET"
   rest_api_id   = "${aws_api_gateway_rest_api.polly.id}"
-
-  /*
-  resource_id   = "${aws_api_gateway_resource.serverless.id}"
-}
-  */
-/*
-resource "aws_api_gateway_resource" "serverless" {
-  parent_id   = "${aws_api_gateway_rest_api.serverless.root_resource_id}"
-  path_part   = "serverlessWebsite"
-  rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
+  resource_id   = "${aws_api_gateway_resource.polly.id}"
 }
 
-resource "aws_api_gateway_integration" "serverless" {
-  http_method             = "${aws_api_gateway_method.serverless.http_method}"
+resource "aws_api_gateway_integration" "polly_get" {
+  http_method             = "${aws_api_gateway_method.polly_get.http_method}"
   integration_http_method = "POST"
-  resource_id             = "${aws_api_gateway_resource.serverless.id}"
-  rest_api_id             = "${aws_api_gateway_rest_api.serverless.id}"
+  resource_id             = "${aws_api_gateway_resource.polly.id}"
+  rest_api_id             = "${aws_api_gateway_rest_api.polly.id}"
   type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.serverless.arn}/invocations"
+  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.polly_get_posts.arn}/invocations"
 }
 
-resource "aws_api_gateway_method_response" "serverless" {
-  http_method = "${aws_api_gateway_method.serverless.http_method}"
-  resource_id = "${aws_api_gateway_resource.serverless.id}"
-  rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
+resource "aws_api_gateway_method_response" "polly_get" {
+  http_method = "${aws_api_gateway_method.polly_get.http_method}"
+  resource_id = "${aws_api_gateway_resource.polly.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.polly.id}"
   status_code = "200"
 
   response_models {
@@ -44,6 +40,7 @@ resource "aws_api_gateway_method_response" "serverless" {
   }
 }
 
+/*
 resource "aws_api_gateway_deployment" "serverless" {
   rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
   stage_name  = "prod"
