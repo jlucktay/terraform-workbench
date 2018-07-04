b18f253fbf9cc980b1ba30830fbe0429602c6fb1
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-07-04 11:57:33 +0100

    Configured variables

--- projects/px1021/teleport-example/terraform/export.sh
+++ projects/px1021/teleport-example/terraform/export.sh
@@ -2,13 +2,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-export TF_VAR_region="us-west-2"
-export TF_VAR_cluster_name=example.com
-export TF_VAR_teleport_version="2.5.0-alpha.4"
-export TF_VAR_key_name="ops"
-export TF_VAR_license_path="/path/to/license"
-export TF_VAR_ami_name="debian-stretch-hvm-x86_64-gp2-2018-01-06-16218-572488bb-fc09-4638-8628-e1e1d26436f4-ami-628ad918.4"
-export TF_VAR_route53_zone="example.com"
-export TF_VAR_route53_domain="teleport.example.com"
-export TF_VAR_s3_bucket_name="teleport.example.com"
-export TF_VAR_email="support@example.com"
+export TF_VAR_region="eu-west-1"
+export TF_VAR_cluster_name="james.lucktaylor.tidal.teleport.demo"
+export TF_VAR_teleport_version="2.6.6"
+export TF_VAR_key_name="james.lucktaylor.${TF_VAR_region}"
+export TF_VAR_license_path="/Users/jameslucktaylor/git/bitbucket.org/cloudreach/james-lucktaylor-terraform/projects/px1021/teleport-example/terraform/license.pem"
+export TF_VAR_route53_zone="celab.cloudreach.com"
+export TF_VAR_route53_domain="tidal.teleport.demo.${TF_VAR_route53_zone}"
+export TF_VAR_s3_bucket_name="${TF_VAR_cluster_name}"
+export TF_VAR_email="james.lucktaylor@cloudreach.com"
