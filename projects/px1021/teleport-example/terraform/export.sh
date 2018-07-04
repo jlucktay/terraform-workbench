55e61cc09319be900be180698ac76873d3a8d02b
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-07-04 19:48:59 +0100

    Fixed bucket name

--- projects/px1021/teleport-example/terraform/export.sh
+++ projects/px1021/teleport-example/terraform/export.sh
@@ -15,7 +15,7 @@ export TF_VAR_key_name="james.lucktaylor.${TF_VAR_region}"
 export TF_VAR_license_path="/Users/jameslucktaylor/git/bitbucket.org/cloudreach/james-lucktaylor-terraform/projects/px1021/teleport-example/terraform/license.pem"
 export TF_VAR_route53_zone="celab.cloudreach.com"
 export TF_VAR_route53_domain="tidal.teleport.demo.${TF_VAR_route53_zone}"
-export TF_VAR_s3_bucket_name="${TF_VAR_cluster_name}"
+export TF_VAR_s3_bucket_name="james.lucktaylor.tidal.teleport.demo"
 export TF_VAR_email="james.lucktaylor@cloudreach.com"
 
 echo "Done!"
