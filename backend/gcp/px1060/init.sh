2bee6044a5ac798b6e6573500fa57b37bf669f51
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-07-24 12:30:26 +0100

    Added an unset for region variable, just in case

--- backend/gcp/px1060/init.sh
+++ backend/gcp/px1060/init.sh
@@ -19,11 +19,14 @@ GCloudOrgId="1034654569623"
 GCloudBillingId="01DA4D-2AA64D-266E55"
 
 # Export the following variables to your environment for use throughout:
-export TF_VAR_org_id=$GCloudOrgId
-export TF_VAR_billing_account=$GCloudBillingId
+export TF_VAR_org_id="$GCloudOrgId"
+export TF_VAR_billing_account="$GCloudBillingId"
 export TF_ADMIN="px1060-terraform-admin"
-export TF_CREDS=~/.config/gcloud/${TF_ADMIN}.json
+export TF_CREDS="$HOME/.config/gcloud/${TF_ADMIN}.json"
 # Note: The TF_ADMIN variable will be used for the name of the Terraform Admin Project and must be unique.
+
+# Region might already be set from elsewhere, so clear it
+unset -v TF_VAR_region
 
 IFS=$PreviousIFS
 
