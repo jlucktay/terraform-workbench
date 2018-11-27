7dd9514a5a36f43576351ed8e591b45f01831ea6
James Lucktaylor <jlucktay+github@gmail.com>
2018-11-27 22:00:23 +0000

    Added new state variables

--- infra/config.tf
+++ infra/config.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 0.11.7"
-
   backend "s3" {
     acl            = "private"
-    bucket         = "james-lucktaylor-terraform"
-    dynamodb_table = "james.lucktaylor.terraform"
+    bucket         = "${var.state_bucket}"
+    dynamodb_table = "${var.state_dynamodb}"
     encrypt        = true
   }
 }
--- infra/var.tf
+++ infra/var.tf
@@ -1,3 +1,11 @@
 variable "region" {
   description = "Which AWS region to operate in?  No default is set, so that prompts will occur"
 }
+
+variable "state_bucket" {
+  description = "The name of the S3 bucket that Terraform state will be stored in"
+}
+
+variable "state_dynamodb" {
+  description = "The name of the DynamoDB table that Terraform state will be locked with"
+}
