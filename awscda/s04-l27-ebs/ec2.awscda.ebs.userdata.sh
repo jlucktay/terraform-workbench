0fa09961afd7e3967dd78a732ecebf2ab364e691
James Lucktaylor <jlucktay+github@gmail.com>
2018-11-29 12:19:15 +0000

    Beefed up script with output for cloud-init logs and more checking.

--- awscda/s04-l27-ebs/ec2.awscda.ebs.userdata.sh
+++ awscda/s04-l27-ebs/ec2.awscda.ebs.userdata.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Drop a note when this script is done (note: 'done' might include exiting prematurely due to an error!)
+trap "echo DONE >> ~ec2-user/user-data.log" INT TERM EXIT
 
 yum update -y
 
 drives=( sdb sdc sdd )
 
-for drive in "${drives[@]}"
-do
+for drive in "${drives[@]}"; do
+    echo -n "Waiting for device '/dev/$drive'..."
+    until ls "/dev/$drive"; do
+        sleep 1
+    done
+    echo " found: $(ls "/dev/$drive")"
+
+    echo -n "Partitioning drive '$drive'..."
     parted --script "/dev/$drive" \
         mklabel gpt \
         mkpart primary ext4 0% 100%
+    echo " 'parted' finished: $?"
 
-    mkfs.ext4 -F "/dev/$drive"
-    mkdir -p "/mnt/$drive"
-    echo "/dev/$drive   /mnt/$drive   ext4   defaults   0   2" | tee -a /etc/fstab
-    mount "/mnt/$drive"
+    echo -n "Waiting for device '/dev/${drive}1'..."
+    until ls "/dev/${drive}1"; do
+        sleep 1
+    done
+    echo " found: $(ls "/dev/${drive}1")"
+
+    echo -n "Creating filesystem on '/dev/${drive}1'..."
+    mkfs -t ext4 "/dev/${drive}1"
+    echo " 'mkfs' finished: $?"
+
+    mkdir -pv "/mnt/${drive}1"
+
+    echo "/dev/${drive}1   /mnt/${drive}1   ext4   defaults   0   2" | tee -a /etc/fstab
+
+    mount "/mnt/${drive}1"
 done
