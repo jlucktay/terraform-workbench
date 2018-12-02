2d0ce4181ed460f9a66b1bf94c2599f6f6eb81fb
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-02 07:09:07 +0000

    Added IOPS for 'io1' volume type

--- awscda/s04-l27-ebs/resource.ec2.tf
+++ awscda/s04-l27-ebs/resource.ec2.tf
@@ -27,26 +27,31 @@ locals {
   volumes = [
     {
       aws_type     = "gp2"
+      iops         = 0
       linux_device = "sdb"
       size         = "1"
     },
     {
       aws_type     = "io1"
+      iops         = 100
       linux_device = "sdc"
       size         = "4"
     },
     {
       aws_type     = "st1"
+      iops         = 0
       linux_device = "sdd"
       size         = "500"
     },
     {
       aws_type     = "sc1"
+      iops         = 0
       linux_device = "sde"
       size         = "500"
     },
     {
       aws_type     = "standard"
+      iops         = 0
       linux_device = "sdf"
       size         = "1"
     },
@@ -56,6 +61,7 @@ locals {
 resource "aws_ebs_volume" "ebs" {
   availability_zone = "${element(aws_instance.awscda.*.availability_zone, count.index)}"
   count             = "${aws_instance.awscda.count * length(local.volumes)}"
+  iops              = "${lookup(local.volumes[count.index], "iops")}"
   size              = "${lookup(local.volumes[count.index], "size")}"
   type              = "${lookup(local.volumes[count.index], "aws_type")}"
 
