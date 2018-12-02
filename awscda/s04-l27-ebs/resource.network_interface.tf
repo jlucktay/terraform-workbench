468c791336e5962ac21126f8fe2d791da8421cd8
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-02 07:20:56 +0000

    More scaling for multiple EC2s

--- awscda/s04-l27-ebs/resource.network_interface.tf
+++ awscda/s04-l27-ebs/resource.network_interface.tf
@@ -1,4 +1,5 @@
 resource "aws_network_interface_sg_attachment" "awscda-dmz" {
-  network_interface_id = "${aws_instance.awscda.primary_network_interface_id}"
+  count                = "${aws_instance.awscda.count}"
+  network_interface_id = "${element(aws_instance.awscda.*.primary_network_interface_id, count.index)}"
   security_group_id    = "${data.aws_security_group.dmz.id}"
 }
