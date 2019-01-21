352af66983208d5a907f7d8b03de4f6790c08bbf
James Lucktaylor <jlucktay+github@gmail.com>
2019-01-21 13:31:31 +0000

    Tune up verbosity, so that 'mage' vs 'mage -v' are actually different

--- terratest/azure/magefile.go
+++ terratest/azure/magefile.go
@@ -5,7 +5,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -18,27 +17,30 @@ import (
 // Default is the default target when the command executes `mage` in Cloud Shell
 var Default = Full
 
-var labID string
-
-func init() {
-	fmt.Println("Looking up lab subscription...")
-	initLabID, errAz := exec.Command("sh", "-c", "az account list 2>/dev/null | jq -r '.[] | select( .name == \"LAB27\" ) | .id'").Output()
-	if errAz != nil {
-		log.Fatal(errAz)
-	}
-	labID = strings.TrimSpace(string(initLabID))
-}
-
 // Full runs Clean, Format, Unit and Integration in sequence
 func Full() {
 	mg.Deps(Unit)
 	mg.Deps(Integration)
 }
 
-// SelectLab selects the correct Azure lab subscription to provision test resources in
+var labID string
+
+// LookupLab finds the GUID of the correct Azure lab subscription to provision test resources in
+func LookupLab() error {
+	fmt.Println("Looking up lab subscription...")
+	rawLabID, errAz := exec.Command("sh", "-c", "az account list 2>/dev/null | jq -r '.[] | select( .name == \"LAB27\" ) | .id'").Output()
+	if errAz != nil {
+		return errAz
+	}
+	labID = strings.TrimSpace(string(rawLabID))
+	return nil
+}
+
+// SelectLab selects the Azure lab subscription
 func SelectLab() error {
+	mg.Deps(LookupLab)
 	fmt.Println("Selecting lab subscription...")
-	return sh.RunV("az", "account", "set", fmt.Sprintf("--subscription=%s", labID))
+	return sh.Run("az", "account", "set", fmt.Sprintf("--subscription=%s", labID))
 }
 
 // Unit runs unit tests
@@ -47,7 +49,7 @@ func Unit() error {
 	mg.Deps(Format)
 	mg.Deps(SelectLab)
 	fmt.Println("Running unit tests...")
-	return sh.RunV("go", "test", "./test/", "-run", "TestUT_", "-v")
+	return sh.Run("go", "test", "./test/", "-run", "TestUT_", "-v")
 }
 
 // Integration runs integration tests
@@ -56,16 +58,16 @@ func Integration() error {
 	mg.Deps(Format)
 	mg.Deps(SelectLab)
 	fmt.Println("Running integration tests...")
-	return sh.RunV("go", "test", "./test/", "-run", "TestIT_", "-v")
+	return sh.Run("go", "test", "./test/", "-run", "TestIT_", "-v")
 }
 
 // Format formats both Terraform code and Go code
 func Format() error {
 	fmt.Println("Formatting...")
-	if err := sh.RunV("terraform", "fmt", "."); err != nil {
+	if err := sh.Run("terraform", "fmt", "."); err != nil {
 		return err
 	}
-	return sh.RunV("go", "fmt", "./test/")
+	return sh.Run("go", "fmt", "./test/")
 }
 
 // Clean removes temporary build and test files
