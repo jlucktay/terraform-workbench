aec7e8d88757785c1490435cf9334c1f25085456
James Lucktaylor <jlucktay+github@gmail.com>
2020-04-30 17:06:11 +0100

    Finalise pre-reqs.

--- rancher/resource.ec2.tf
+++ rancher/resource.ec2.tf
@@ -4,12 +4,13 @@ resource "aws_instance" "main" {
   ami                         = data.aws_ami.ubuntu.id
   associate_public_ip_address = true
   availability_zone           = element(data.aws_availability_zones.available.names, count.index)
+  disable_api_termination     = false
   instance_type               = "t2.large"
   key_name                    = "jlucktay.eu-west-2"
   subnet_id                   = element(data.aws_subnet.public.*.id, count.index)
   user_data                   = file(join("/", [path.module, "user-data.sh"]))
 
-  security_groups = [
+  vpc_security_group_ids = [
     data.aws_security_group.dmz.id
   ]
 
--- rancher/user-data.sh
+++ rancher/user-data.sh
@@ -3,9 +3,8 @@ set -euxo pipefail
 shopt -s globstar nullglob
 IFS=$'\n\t'
 
-apt-get update
-apt upgrade --assume-yes
-apt autoremove --assume-yes
+# Boilerplate - update/upgrade
+apt-get update && apt-get upgrade --assume-yes && apt-get autoremove --assume-yes
 
 # https://docs.docker.com/engine/install/ubuntu/
 
@@ -29,7 +28,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 # Verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88,
 # by searching for the last 8 characters of the fingerprint
-if ! verify_key=$(apt-key fingerprint 0EBFCD88 | grep --count "9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88"); then
+export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
+
+if ! verify_key=$(apt-key fingerprint 0EBFCD88 \
+  | grep --count "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"); then
   exit 1
 fi
 
@@ -60,13 +62,13 @@ docker run hello-world
 ## Manage Docker as a non-root user
 
 ### Create the docker group
-groupadd docker
+groupadd docker || true
 
 # +++ Users already present
 while IFS= read -r line; do
   user_id=$(cut --delimiter=":" --fields=3 <<< "$line")
 
-  if ((user_id > 1000)) && ((user_id < 65534)); then
+  if ((user_id >= 1000)) && ((user_id < 65534)); then
     user_name=$(cut --delimiter=":" --fields=1 <<< "$line")
     usermod --append --groups docker "$user_name"
   fi
@@ -80,3 +82,8 @@ echo 'ADD_EXTRA_GROUPS=1' | tee --append /etc/adduser.conf
 
 ### systemd
 systemctl enable docker
+
+# https://rancher.com/quick-start/
+docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
+
+reboot
